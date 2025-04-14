ce5d7e19dbe310cb49ce6a5a3aab7501cfc78da6
juaca2922 <joaquinfernandezmalca2922@gmail.com>
2025-04-13 23:11:10 -0500

    Agregue todo lo del orignal

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")  # Esto carga la página principal
-
+@app.route('/cotizar')
+def cotizar():
+    return render_template('cotizar.html')
 if __name__ == "__main__":
     # Obtiene el puerto asignado por Render, si no existe usa el puerto 5000 por defecto
     port = int(os.environ.get("PORT", 5000))
