cfb1005bdfb121ba202003116a5a36a3b4ba07fc
juaca2922 <joaquinfernandezmalca2922@gmail.com>
2025-04-13 22:46:23 -0500

    Agrega app.py configurado para Render

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html")  # Esto carga la página principal
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Obtiene el puerto asignado por Render, si no existe usa el puerto 5000 por defecto
+    port = int(os.environ.get("PORT", 5000))
+
+    # Ejecuta el servidor en 0.0.0.0 para
