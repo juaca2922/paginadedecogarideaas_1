7ef69199803e43033aacc2b2d5de58507ddbe2d9
juaca2922 <joaquinfernandezmalca2922@gmail.com>
2025-04-18 01:24:31 -0500

    cambios en los tips de maceta vela etc

--- app.py
+++ app.py
@@ -35,6 +35,37 @@ def respuesta_delivery():
 def respuesta_modelos():
     return render_template('respuesta_modelos.html')
 
+
+@app.route('/cotizar/<producto_seleccionado>')
+def cotizar_producto(producto_seleccionado):
+    # Usamos un nombre más sencillo para el archivo, asegurándonos de que no haya caracteres especiales
+    template_name = f'cotizar_{producto_seleccionado}.html'
+
+    # Validamos si la plantilla existe antes de intentar cargarla
+    try:
+        return render_template(template_name)
+    except:
+        # Si no existe la plantilla, mostramos un error 404
+        return "Error: El producto seleccionado no existe", 404
+
+
+@app.route("/cotizar/macetas")
+def cotizar_macetas():
+    return render_template('cotizar_macetas.html')
+@app.route('/cotizar/macetas_de_loza')
+def cotizar_macetas_de_loza():
+    return render_template('cotizar_macetas_de_loza.html')
+@app.route('/cotizar/macetas_de_arcilla')
+def cotizar_macetas_de_arcilla():
+    return render_template('cotizar_macetas_de_arcilla.html')
+@app.route('/cotizar_velas')
+def cotizar_velas():
+    return render_template('cotizar_velas.html')
+@app.route('/cotizar_velas_tubo')
+def cotizar_velas_tubo():
+    return render_template('cotizar_velas_tubo.html')
+
+
 if __name__ == "__main__":
     # Obtiene el puerto asignado por Render, si no existe usa el puerto 5000 por defecto
     port = int(os.environ.get("PORT", 5000))
